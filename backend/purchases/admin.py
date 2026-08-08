2cdf3e4e0c9f5e65f6d9d9cbf5ec86f33567f813
petrosgeto7-art <petrosgeto7@gmail.com>
2026-08-08 23:50:12 +0300

    Add Purchase admin with inline PurchaseItems

--- backend/purchases/admin.py
+++ backend/purchases/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 from .models import Purchase, PurchaseItem
 
+
 class PurchaseItemInline(admin.TabularInline):
     model = PurchaseItem
-    extra = 1
+    extra = 0
+
 
 @admin.register(Purchase)
 class PurchaseAdmin(admin.ModelAdmin):
-    list_display = ['purchase_number', 'supplier', 'date', 'total', 'status', 'payment_status']
-    list_filter = ['status', 'payment_status', 'date']
-    search_fields = ['purchase_number', 'supplier__name']
+    list_display = ('purchase_number', 'supplier', 'total', 'status', 'payment_status', 'date', 'created_by')
+    list_filter = ('status', 'payment_status')
+    search_fields = ('purchase_number', 'supplier__name')
+    ordering = ('-date',)
     inlines = [PurchaseItemInline]
-    date_hierarchy = 'date'
