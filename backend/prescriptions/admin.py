8eb4afad9b75fc68d26df0eece37fa7206d19b3f
petrosgeto7-art <petrosgeto7@gmail.com>
2026-08-08 23:50:29 +0300

    Register Prescription and Expense models in admin

--- backend/prescriptions/admin.py
+++ backend/prescriptions/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 from .models import Prescription, PrescriptionItem
 
+
 class PrescriptionItemInline(admin.TabularInline):
     model = PrescriptionItem
-    extra = 1
+    extra = 0
+
 
 @admin.register(Prescription)
 class PrescriptionAdmin(admin.ModelAdmin):
-    list_display = ['prescription_number', 'customer', 'doctor_name', 'prescription_date', 'status']
-    list_filter = ['status', 'prescription_date']
-    search_fields = ['prescription_number', 'customer__name', 'doctor_name']
+    list_display = ('prescription_number', 'customer', 'doctor_name', 'status', 'prescription_date', 'dispensed_by')
+    list_filter = ('status',)
+    search_fields = ('prescription_number', 'customer__name', 'doctor_name')
+    ordering = ('-prescription_date',)
     inlines = [PrescriptionItemInline]
-    date_hierarchy = 'prescription_date'
